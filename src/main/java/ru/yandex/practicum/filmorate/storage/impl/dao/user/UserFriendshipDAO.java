0d14b116b89857ba2e099d10cf9febee6ca5abca
Andrey Rastorguev <parus.remote@yandex.ru>
2023-03-30 06:44:58 +0200

    final_11_13

--- src/main/java/ru/yandex/practicum/filmorate/storage/impl/dao/user/UserFriendshipDAO.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/impl/dao/user/UserFriendshipDAO.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.FriendshipStorage;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -19,7 +18,7 @@ public class UserFriendshipDAO implements FriendshipStorage {
 
     public void saveAll(User user) {
         Map<Integer, Boolean> friendships = user.allFriendships();
-        for (Integer friendId: friendships.keySet()) {
+        for (Integer friendId : friendships.keySet()) {
             String sqlFriendship = "INSERT INTO friendships (friend_from_id, friend_to_id, is_accept) VALUES (?, ?, ?);";
             jdbcTemplate.update(sqlFriendship, user.getId(), friendId, friendships.get(friendId));
         }
@@ -28,12 +27,12 @@ public class UserFriendshipDAO implements FriendshipStorage {
     public void updateAll(User user) {
         Map<Integer, Boolean> dbFriendships = load(user.getId());
         Map<Integer, Boolean> friendships = user.allFriendships();
-        for (int friendId: friendships.keySet()) {
+        for (int friendId : friendships.keySet()) {
             if (!dbFriendships.containsKey(friendId)) {
                 insertFriendship(user.getId(), friendId, friendships.get(friendId));
             }
         }
-        for (int dbFriendId: dbFriendships.keySet()) {
+        for (int dbFriendId : dbFriendships.keySet()) {
             if (!friendships.containsKey(dbFriendId)) {
                 remove(user.getId(), dbFriendId);
             } else if (friendships.get(dbFriendId) != dbFriendships.get(dbFriendId)) {
@@ -44,9 +43,9 @@ public class UserFriendshipDAO implements FriendshipStorage {
 
     public Map<Integer, Boolean> load(int userId) {
         String sql = "SELECT friend_to_id, is_accept FROM friendships WHERE friend_from_id = ?;";
-        List<Integer> keys = jdbcTemplate.query(sql, (rs, rowNum) -> rs.getInt("friend_to_id"),userId)
+        List<Integer> keys = jdbcTemplate.query(sql, (rs, rowNum) -> rs.getInt("friend_to_id"), userId)
                 .stream().collect(Collectors.toList());
-        List<Boolean> values = jdbcTemplate.query(sql, (rs, rowNum) -> rs.getBoolean("is_accept"),userId)
+        List<Boolean> values = jdbcTemplate.query(sql, (rs, rowNum) -> rs.getBoolean("is_accept"), userId)
                 .stream().collect(Collectors.toList());
         return IntStream.range(0, keys.size()).boxed().collect(Collectors.toMap(keys::get, values::get));
 
