82d80772f1a1b3fec19ec808bf009f047541ee9f
Andrey Rastorguev <parus.remote@yandex.ru>
2023-03-31 14:10:07 +0200

    final_11_fixed_02

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
